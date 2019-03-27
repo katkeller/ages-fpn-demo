d52494c5fae21255010cde3b83e9b847971416c1
Kat Keller <kat.kek.keller@gmail.com>
2019-03-26 23:58:08 -0500

    Added button sound effect to interactive object prefab
    Finished interactive objects tutorial 4

--- Assets/Scripts/InteractiveObject.cs
+++ Assets/Scripts/InteractiveObject.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
     [SerializeField]
     private string displayText = nameof(InteractiveObject);
+
     public string DisplayText => displayText;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void InteractWith()
     {
+        audioSource.Play();
         Debug.Log($"Player just interacted with {gameObject.name}!");
     }
 }
