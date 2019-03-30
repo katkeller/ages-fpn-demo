0cc72148011d035b962e7b88d24e4167a961035c
Kat Keller <kat.kek.keller@gmail.com>
2019-03-30 16:52:36 -0500

    Added light flicker script to main menu scene, and added new assets
    Assets added: door, audio, fonts.

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject light;
+
+    [SerializeField]
+    private float timer;
+
+    private void Start()
+    {
+        StartCoroutine(FlickeringLight());
+    }
+
+    IEnumerator FlickeringLight()
+    {
+        light.SetActive(true);
+        timer = Random.Range(0.1f, 5);
+        yield return new WaitForSeconds(timer);
+        light.SetActive(false);
+        timer = Random.Range(0.1f, 1);
+        yield return new WaitForSeconds(timer);
+        StartCoroutine(FlickeringLight());
+    }
+}
