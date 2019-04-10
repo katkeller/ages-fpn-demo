24e6110fa316d0536451ac927d3f8b414abbdfc6
Kat Keller <kat.kek.keller@gmail.com>
2019-04-09 22:42:28 -0500

    Added scripting that makes inventory objects dissapear while still being accessible to the player through inventory
    got to 41 minutes in the inventory object tutorial

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -7,4 +7,31 @@ public class InventoryObject : InteractiveObject
     // TODO: Add long description field
     // TODO: Add Icon field
 
+    private new Renderer renderer;
+    private new Collider collider;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        collider = GetComponent<Collider>();
+    }
+
+    public InventoryObject()
+    {
+        displayText = nameof(InventoryObject);
+    }
+
+    /// <summary>
+    /// When the player interacts with an inventory object, we must:
+    /// 1. Add inventory object to PlayerInventory list
+    /// 2. Remove the object from the game world and makle sure the player cannot interact with it while still being able to access it in inventory
+    /// </summary>
+    public override void InteractWith()
+    {
+        base.InteractWith();
+        PlayerInventory.InventoryObjects.Add(this);
+        collider.enabled = false;
+        renderer.enabled = false;
+
+    }
 }
