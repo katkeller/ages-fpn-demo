6dc929c3bb8d9a0a3ee7ffef5a97db5b9de47708
Kat Keller <kat.kek.keller@gmail.com>
2019-04-17 18:17:41 -0500

    Added sticky note icon and description

--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -8,11 +8,16 @@ public class InventoryObject : InteractiveObject
     [SerializeField]
     private string objectName = nameof(InventoryObject);
 
-    // TODO: Add long description field
-    // TODO: Add Icon field
+    [Tooltip("The text that will display when the player selects this object in the inventory menu.")]
+    [TextArea(3, 6)]
+    [SerializeField]
+    private string description;
+
+    [Tooltip("Icon to display for this item in the inventory menu.")]
+    [SerializeField]
+    private Sprite icon;
 
     public string ObjectName => objectName;
-
     private new Renderer renderer;
     private new Collider collider;
 
