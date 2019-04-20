a6bb82d39ff8d086f6c432dc4190d31cb6a9420b
Kat Keller <kat.kek.keller@gmail.com>
2019-04-20 17:30:10 -0500

    Created InventoryMenu script and added it to the InventoryMenuCanvas
    Finished tutorial 21

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            if (instance = null)
+                throw new System.Exception("There is currently no InventoryMenu instance, but you are trying to access it.");
+            return instance;
+        }
+
+        private set { instance = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance = null)
+            instance = this;
+        else
+            throw new System.Exception("There is already an instance of InventoryMenu, and there can only be one.");
+    }
+}
