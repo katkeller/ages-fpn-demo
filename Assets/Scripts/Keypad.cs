7bbbcbe3a693b266b898fb538aa2f74d08012427
katkeller <kat.kek.keller@gmail.com>
2019-04-25 17:54:33 -0500

    Keypad now works with phone recorder!

--- Assets/Scripts/Keypad.cs
+++ Assets/Scripts/Keypad.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Keypad : MonoBehaviour
 {
@@ -16,81 +18,138 @@ public class Keypad : MonoBehaviour
     private bool isEntered;
     private bool clipShouldPlay;
     private AudioSource audioSource;
+    private CanvasGroup canvasGroup;
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
 
+    public static event Action InputEntered;
+
+    private bool IsVisible => canvasGroup.alpha > 0;
+
+    public string Input => input;
+
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        rigidbodyFirstPersonController = FindObjectOfType<RigidbodyFirstPersonController>();
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
         ClearInput();
+        HideMenu();
+        StartCoroutine(WaitForAudioClip());
     }
 
-    public void AddZeroToInput()
+    public void AddZeroToInput(string number)
     {
-        currentInput = currentInput + "0";
+        currentInput = currentInput + number;
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddOneToInput()
     {
         currentInput = currentInput + "1";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddTwoToInput()
     {
         currentInput = currentInput + "2";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddThreeToInput()
     {
         currentInput = currentInput + "3";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddFourToInput()
     {
         currentInput = currentInput + "4";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddFiveToInput()
     {
         currentInput = currentInput + "5";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddSixToInput()
     {
         currentInput = currentInput + "6";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddSevenToInput()
     {
         currentInput = currentInput + "7";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddEightToInput()
     {
         currentInput = currentInput + "8";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void AddNineToInput()
     {
         currentInput = currentInput + "9";
         inputText.text = currentInput;
+        audioSource.Play();
     }
 
     public void EnterInput()
     {
         input = currentInput;
+        audioSource.Play();
+        InputEntered?.Invoke();
+        HideMenu();
     }
 
     public void ClearInput()
     {
         currentInput = "";
         inputText.text = currentInput;
+        audioSource.Play();
+    }
+
+    public void ShowMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        rigidbodyFirstPersonController.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        audioSource.Play();
+    }
+
+    public void HideMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidbodyFirstPersonController.enabled = true;
+        audioSource.Play();
+    }
+
+    private IEnumerator WaitForAudioClip()
+    {
+        float originalVolume = audioSource.volume;
+        audioSource.volume = 0;
+        yield return new WaitForSeconds(audioSource.clip.length);
+        audioSource.volume = originalVolume;
     }
 }
--- Assets/Scripts/PhoneRecorder.cs
+++ Assets/Scripts/PhoneRecorder.cs
@@ -20,15 +20,23 @@ public class PhoneRecorder : MonoBehaviour, IInteractive
     [SerializeField]
     private float clip1AnimationDelay = 2.0f, clip2AnimationDelay = 2.0f, clip2SecondAnimationDelay = 0.0f;
 
+    [SerializeField]
+    private Keypad keypad;
+
+    [SerializeField]
+    private string phoneNumber1, phoneNumber2, phoneNumber3;
+
     public static event Action<Door> DoorAnimationShouldPlay;
 
     public string DisplayText => displayText;
     private AudioSource audioSource;
     private int shouldOpenAnimParameter = Animator.StringToHash("shouldOpen");
     private AudioClip clipToPlay;
+    private float clipToPlayAnimationDelay= 0.0f;
     private Animator otherObjectAnimator;
     private AudioSource otherObjectAudioSource;
     private Door currentDoorToAnimate;
+   
 
     private void Awake()
     {
@@ -40,11 +48,47 @@ public class PhoneRecorder : MonoBehaviour, IInteractive
     private void Start()
     {
         //Placeholder initialization for code that chooses which clip to play based on number dialed
-        clipToPlay = clip1;
-        currentDoorToAnimate = doorToAnimate1;
+        //clipToPlay = clip1;
+        //currentDoorToAnimate = doorToAnimate1;
     }
 
     public void InteractWith()
+    {
+        keypad.ShowMenu();
+
+        //PlayClip();
+        Debug.Log($"Player just interacted with {gameObject.name}!");
+    }
+
+    private void OnInputEntered()
+    {
+        if (keypad.Input == phoneNumber1)
+        {
+            clipToPlay = clip1;
+            currentDoorToAnimate = doorToAnimate1;
+            clipToPlayAnimationDelay = clip1AnimationDelay;
+
+        }
+        else if(keypad.Input == phoneNumber2)
+        {
+            clipToPlay = clip2;
+            currentDoorToAnimate = doorToAnimate2;
+        }
+
+        PlayClip();
+    }
+
+    private void OnEnable()
+    {
+        Keypad.InputEntered += OnInputEntered;
+    }
+
+    private void OnDisable()
+    {
+        Keypad.InputEntered -= OnInputEntered;
+    }
+
+    private void PlayClip()
     {
         try
         {
@@ -55,12 +99,11 @@ public class PhoneRecorder : MonoBehaviour, IInteractive
             throw new System.Exception("Missing AudioSource componant or audio clip. InteractiveObject requires an AudioSource componant and an audio clip.");
         }
         StartCoroutine(PlayAnimation());
-        Debug.Log($"Player just interacted with {gameObject.name}!");
     }
 
     IEnumerator PlayAnimation()
     {
-        yield return new WaitForSeconds(clip1AnimationDelay);
+        yield return new WaitForSeconds(clipToPlayAnimationDelay);
         //otherObjectAnimator.SetBool(shouldOpenAnimParameter, true);
         //otherObjectAudioSource.Play();
         DoorAnimationShouldPlay?.Invoke(currentDoorToAnimate);
