9e7fbcd93d3400c00d5ac27ce548a436906646d7
katkeller <kat.kek.keller@gmail.com>
2019-05-02 17:10:20 -0500

    Added audio visualizer scripts and created spooky ghost light

---
+++ Assets/Scripts/AudioSampleCollector.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSampleCollector : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public static float[] audioBand = new float[8];
+    public static float[] audioBandBuffer = new float[8];
+    private float[] samples = new float[512];
+    private float[] frequencyBand = new float[8];
+    private float[] bandBuffer = new float[8];
+    private float[] bufferDecrease = new float[8];
+    private float[] frequencyBandHighest = new float[8];
+    private float highDecreaseSpeed = 1.2f;
+    private float lowDecreaseSpeed = 0.005f;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        FrequencyBandBuffer();
+        CreateAudioBands();
+    }
+
+    private void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (frequencyBand[i] > frequencyBandHighest[i])
+            {
+                frequencyBandHighest[i] = frequencyBand[i];
+            }
+
+            audioBand[i] = (frequencyBand[i] / frequencyBandHighest[i]);
+            audioBandBuffer[i] = (bandBuffer[i] / frequencyBandHighest[i]);
+        }
+    }
+
+    private void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    private void FrequencyBandBuffer()
+    {
+        for (int g = 0; g < 8; ++g)
+        {
+            if (frequencyBand[g] > bandBuffer[g])
+            {
+                bandBuffer[g] = frequencyBand[g];
+                bufferDecrease[g] = lowDecreaseSpeed;
+            }
+            else if (frequencyBand[g] < bandBuffer[g])
+            {
+                bandBuffer[g] -= bufferDecrease[g];
+                bufferDecrease[g] *= highDecreaseSpeed;
+            }
+        }
+    }
+
+    private void MakeFrequencyBands()
+    {
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            frequencyBand[i] = average * 10;
+        }
+    }
+}
---
+++ Assets/Scripts/AudioVisualizer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisualizer : MonoBehaviour
+{
+    [SerializeField]
+    private float minLightIntensity = 0.0f, maxLightIntensity = 5.0f;
+
+    [Tooltip("Enter a number between 0-7 to choose which of the 8 frequency bands will be sampled from the AudioSampleCollector script.")]
+    [SerializeField]
+    private int frequencyBandToSample;
+
+    [SerializeField]
+    private bool useBuffer;
+
+    [SerializeField]
+    private Light lightSource;
+
+    private Material material;
+
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().materials[0];
+    }
+
+    private void Update()
+    {
+        UpdateLightIntensity();
+        //UpdateMaterial();
+    }
+
+    //private void UpdateMaterial()
+    //{
+    //    if (useBuffer)
+    //    {
+    //        Color color = new Color(AudioSampleCollector.audioBandBuffer[frequencyBandToSample], AudioSampleCollector.audioBandBuffer[frequencyBandToSample], AudioSampleCollector.audioBandBuffer[frequencyBandToSample]);
+    //        material.SetColor("_EmissionColor", color);
+    //    }
+    //    else if (!useBuffer)
+    //    {
+    //        Color color = new Color(AudioSampleCollector.audioBand[frequencyBandToSample], AudioSampleCollector.audioBand[frequencyBandToSample], AudioSampleCollector.audioBand[frequencyBandToSample]);
+    //        material.SetColor("_EmissionColor", color);
+    //    }
+    //}
+
+    private void UpdateLightIntensity()
+    {
+        lightSource.intensity = (AudioSampleCollector.audioBandBuffer[frequencyBandToSample] * (maxLightIntensity - minLightIntensity)) + minLightIntensity;
+    }
+}
