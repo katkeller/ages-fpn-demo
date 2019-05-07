c9ca754d7bb9a93ecf8c75424aaffc4d9896b526
Kat Keller <kat.kek.keller@gmail.com>
2019-05-06 20:05:08 -0500

    Added footsteps when player moves

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Footsteps : MonoBehaviour
+{
+    private CharacterController characterController;
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        rigidbodyFirstPersonController = GetComponent<RigidbodyFirstPersonController>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(rigidbodyFirstPersonController.Grounded == true && rigidbodyFirstPersonController.Velocity.magnitude > 2.0f && audioSource.isPlaying == false)
+        {
+            audioSource.volume = Random.Range(0.8f, 1.0f);
+            audioSource.pitch = Random.Range(0.8f, 1.1f);
+            audioSource.Play();
+        }
+    }
+}
